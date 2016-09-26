e7caab358f4b5c5f793a90a49b5f7304f1dc1e4b
Abe Voelker <abe@abevoelker.com>
2016-09-25 21:14:25 -0500

    Add test for empty template dir and chmod after S3 download

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,11 +5,15 @@ set -eu
 if [ -n "${NGINX_CONF_S3_URI-}" ] ; then
   echo "Downloading config files from $NGINX_CONF_S3_URI ..."
   aws s3 sync $NGINX_CONF_S3_URI /data/conf/
+  chmod -R 644 /data/conf/
 fi
 if [ -n "${NGINX_SITES_S3_URI-}" ] ; then
   echo "Downloading site files from $NGINX_SITES_S3_URI ..."
   aws s3 sync $NGINX_SITES_S3_URI /data/sites-templates/
+  chmod -R 644 /data/sites-templates/
 fi
 
-render-templates.sh /data/sites-templates /data/sites-enabled
+if [ "$(ls -A /data/sites-templates)" ]; then
+  render-templates.sh /data/sites-templates /data/sites-enabled
+fi
 exec $@
